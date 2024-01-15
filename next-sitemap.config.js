242e50cf2768372e1a11c181247ed4f9044a1fc7
Akali <ngoinhamaam1993@gmail.com>
2024-01-15 11:34:51 +0700

    update sitemap, robots

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
-  generateRobotsTxt: true, // (optional)
-  //   sitemapSize: 7000,
+  // generateRobotsTxt: true, // (optional)
+  // sitemapSize: 7000,
+  generateIndexSitemap: false,
   // robotsTxtOptions: {
   //   additionalSitemaps: [
   //     'https://online-card.vercel.app/server-sitemap.xml', // <==== Add here
