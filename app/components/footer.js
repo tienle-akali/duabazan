e91bab8dc5b291e6f6650c5b48cd02c50b4e0bb8
Akali <ngoinhamaam1993@gmail.com>
2024-01-16 17:43:26 +0700

    update messenger

--- app/components/footer.js
+++ app/components/footer.js
@@ -1,7 +1,7 @@
 "use client";
 import { Box, Button, Container, Toolbar, Typography } from "@mui/material";
 import Link from "next/link";
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
 import { topMenu } from "./appbar";
 
 function Copyright() {
@@ -62,10 +62,47 @@ function EmbedFacebook() {
   );
 }
 
+function MessengerPlugin() {
+  useEffect(() => {
+    var chatbox = document.getElementById("fb-customer-chat");
+    chatbox.setAttribute("page_id", "190412854155748");
+    chatbox.setAttribute("attribution", "setup_tool");
+
+    window.fbAsyncInit = function () {
+      FB.init({
+        appId: "886774486482112",
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: "v12.0",
+      });
+    };
+
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  }, []);
+
+  return (
+    <>
+      {/* <!-- Messenger Chat Plugin Code --> */}
+      <div id="fb-root"></div>
+
+      <div id="fb-customer-chat" className="fb-customerchat"></div>
+    </>
+  );
+}
+
 function MainFooter() {
   return (
     <footer>
       <Container>
+        <MessengerPlugin />
         <FooterMenu />
         <EmbedFacebook />
         <Toolbar sx={{ justifyContent: "center" }}>
